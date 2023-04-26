174b13952c9bc50c770604e42cb4354d0a204ca4
Kreyno93 <michael.wagener@neuefische.de>
2023-04-26 22:44:51 +0200

    newst newest

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import java.time.Instant;
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
         new Michael().wantsMcFlurry();
+
     }
 }
